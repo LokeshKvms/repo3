e0a70f37bd94d254ff2e1f8959bb759ed915f4f0
LokeshKvms <loki.kvms@gmail.com>
2026-05-06 22:20:03 +0530

    media

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 def function_cicd(request):
-    """Responds to any HTTP request.
-    Args:
-        request (flask.Request): HTTP request object.
-    Returns:
-        The response text or any set of values that can be turned into a
-        Response object using
-        `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`
-    """
+    request_json = request.get_json(silent=True)
 
-    request_json = request.get_json()
-    if request.args and 'message' in request.args:
+    if request.args.get('message'):
         return request.args.get('message')
-    elif request_json and 'message' in request_json:
+
+    if request_json and 'message' in request_json:
         return request_json['message']
-    else:
-        return f'Function - 1 with V1.0'
+
+    # fallback: raw body
+    data = request.data.decode('utf-8')
+    if data:
+        return data
+
+    return 'Function - 1 with V1.0'
